89238e37553b343600b6be153cc4d61e9846d2c5
djangojazz <brettmorin2@gmail.com>
2017-01-12 14:27:05 -0800

    Main Branch start

---
+++ MoneyEntry/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MoneyEntry;
+using MoneyEntry.DataAccess;
+using MoneyEntry.Model;
+using System.Linq;
+using MoneyEntry.ViewModel;
+
+namespace MoneyEntry
+{
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
