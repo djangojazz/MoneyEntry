a8d6337b6adc9e07ffc9b7b5c59f0b9f3bf1dba2
djangojazz <brettmorin2@gmail.com>
2017-03-22 06:30:39 -0700

    Added some models for the charting portion

---
+++ Controls/Charting/Models/PlotPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls.Charting
+{
+  public class PlotPoint<T> : PlotPoint
+  {
+    public PlotPoint(T point)
+    {
+      if ((typeof(T) != typeof(decimal)) || (typeof(T) != typeof(double)) || (typeof(T) != typeof(int)) || (typeof(T) != typeof(DateTime)) || (typeof(T) != typeof(DateTime)))
+        throw new ArgumentException("Cannot have a plot point that is not Decimal, Double, Integer, Date or DateTime");
+
+      this.Point = point;
+    }
+    public T Point { get; set; }
+
+    public override Type PointType
+    {
+      get { return typeof(T); }
+    }
+  }
+
+  public abstract class PlotPoint
+  {
+    public abstract Type PointType { get; }
+  }
+}
---
+++ Controls/Charting/Models/PlotPoints.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+namespace Controls.Charting
+{
+  public sealed class PlotPoints
+  {                            
+    public PlotPoint X { get; }
+    public PlotPoint Y { get; }
+
+    public PlotPoints(PlotPoint x, PlotPoint y)
+    {
+      this.X = x;
+      this.Y = y;
+    }
+
+    public double XAsDouble
+    {
+      get
+      {
+        if ((X.PointType == typeof(int)) || (X.PointType == typeof(double)) || (X.PointType == typeof(decimal)))
+          return Convert.ToDouble(((PlotPoint<int>)X).Point);
+        else if (X.PointType == typeof(DateTime))
+          return Convert.ToDouble(((PlotPoint<DateTime>)X).Point.Ticks);
+        else
+          return 0;
+      }
+    }
+
+    public double YAsDouble
+    {
+      get
+      {
+        if ((Y.PointType == typeof(int)) || (Y.PointType == typeof(double)) || (Y.PointType == typeof(decimal)))
+          return Convert.ToDouble(((PlotPoint<int>)Y).Point);
+        else if (Y.PointType == typeof(DateTime))
+          return Convert.ToDouble(((PlotPoint<DateTime>)Y).Point.Ticks);
+        else
+          return 0;     
+      }
+    }
+
+    public override string ToString()
+    {
+      return $"X {XAsDouble} Y {YAsDouble}";
+    } 
+  }
+}
