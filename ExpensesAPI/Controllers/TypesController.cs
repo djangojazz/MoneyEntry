51c1a139cc7b0220ce6e589fc278d703166787ef
djangojazz <brettmorin2@gmail.com>
2017-11-17 06:20:23 -0800

    Added Expenses API

---
+++ ExpensesAPI/Controllers/TypesController.cs
@@ -0,0 +1,47 @@
+﻿using MoneyEntry.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpensesAPI.Controllers
+{
+    public class TypesController : ApiController
+    {
+        private DataRetreival _dataRetreival;
+
+        public TypesController()
+        {
+            _dataRetreival = DataRetreival.Instance;
+        }
+
+        // GET api/types
+        public IEnumerable<tdType> Get()
+        {
+            return _dataRetreival.GetTypes();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MoneyEntry.DataAccess/Expenses.Context.cs
+++ MoneyEntry.DataAccess/Expenses.Context.cs
@@ -21,11 +21,6 @@ namespace MoneyEntry.DataAccess
         public ExpensesEntities() : base("name=ExpensesEntities") { }
 
         public ExpensesEntities(string connection) : base(connection) { }
-        
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            throw new UnintentionalCodeFirstException();
-        }
     
         public virtual DbSet<tdCategory> tdCategory { get; set; }
         public virtual DbSet<tdType> tdType { get; set; }
