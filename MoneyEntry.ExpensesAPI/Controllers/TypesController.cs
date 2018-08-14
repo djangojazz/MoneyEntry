4ce11dc93b2184f91c7aa17c887de816293c0994
djangojazz <brettmorin2@gmail.com>
2018-08-14 08:38:34 -0700

    Added Authorization attribute to test Types controller

--- MoneyEntry.ExpensesAPI/Controllers/TypesController.cs
+++ MoneyEntry.ExpensesAPI/Controllers/TypesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MoneyEntry.DataAccess.EFCore;
 
 namespace MoneyEntry.ExpensesAPI.Controllers
 {
-    [Route("expensesApi/[controller]/[action]")]
+    [Route("expensesApi/[controller]/[action]"), Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class TypesController : Controller
     {
         ExpensesRepository _repo = ExpensesRepository.Instance;
